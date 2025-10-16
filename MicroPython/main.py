b753e04dd7a4a34d90ba4951ebd024c418cb1ed4
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-10-16 02:04:30 +0000

    if else statements in python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,37 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Isaac Ip
+Created on: Oct 2025
 This module is a Micro:bit MicroPython program
+This program uses if, else statements.
 """
 
 from microbit import *
+import random
 
+# variables
+integer1 = 0
+integer2 = 0
 
-display.scroll("Hello, World!")
+# cleanup
+display.clear()
+display.show(Image.HAPPY)
+
+# set the variables on loadup
+integer1 = random.randint(0, 99)
+integer2 = random.randint(0, 99)
+
+while True:
+    if button_a.is_pressed():
+        display.show("#1: " + integer1)
+        display.show(Image.HAPPY)
+
+    if button_b.is_pressed():
+        display.show("#2: " + integer2)
+        display.show(Image.HAPPY)
+
+    if accelerometer.was_gesture("shake"):
+        if integer1 > integer2:
+            display.show(integer1 + " > " + integer2)
+        else:
+            display.show(integer1 + " < " + integer2)
+        display.show(Image.HAPPY)
