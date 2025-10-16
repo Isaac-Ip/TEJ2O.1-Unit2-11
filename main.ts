46b4087d8d9b7115fa7f1e79fc72c3e114753e84
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-10-15 21:47:28 -0400

    if else statements

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 Isaac Ip All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Isaac Ip
+ * Created on: Oct 2025
+ * This program uses if, else statements.
 */
 
-basic.showString('Hello, World!')
+// variables
+let integer1: number = 0
+let integer2: number = 0
+
+// cleanup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// set the variables on loadup
+integer1 = randint(0, 99)
+integer2 = randint(0, 99)
+
+// show integer 1
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showString("#1: ")
+    basic.showNumber(integer1)
+    basic.showIcon(IconNames.Happy)
+})
+
+// show integer 2
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("#2: ")
+    basic.showNumber(integer2)
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onGesture(Gesture.Shake, function() {
+    basic.clearScreen()
+    basic.showNumber(integer1)
+    if (integer1 > integer2)
+        basic.showString(" > ")
+    else
+        basic.showString(" < ")
+    basic.showNumber(integer2)
+    basic.showIcon(IconNames.Happy)
+})
